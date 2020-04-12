808d35998c4e4e41025c44bede3b91804bba9a41
kenken-yasuken <kenken.yasuken@gmail.com>
2020-04-13 01:30:33 +0900

    migration

---
+++ database/migrations/2020_04_12_161227_create_matchings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matchings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('trainer_id');
+            $table->integer('trainee_id');
+            $table->integer('sport_id');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matchings');
+    }
+}
