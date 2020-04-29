d665113ee82d2bfd17c53fc06afefab301b7aeee
kenken-yasuken <kenken.yasuken@gmail.com>
2020-04-29 23:54:47 +0900

    add table

---
+++ database/migrations/2020_04_29_145145_create_user_actions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_actions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('from_user_id');
+            $table->string('to_user_id');
+            $table->string('action_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_actions');
+    }
+}
