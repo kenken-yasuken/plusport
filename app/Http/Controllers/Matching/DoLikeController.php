fb35733da8d84b8e5b2a5e3974f9a79bdfbda625
kenken-yasuken <kenken.yasuken@gmail.com>
2020-06-08 01:58:34 +0900

    insert matching

--- app/Http/Controllers/Matching/DoLikeController.php
+++ app/Http/Controllers/Matching/DoLikeController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\View;
 use App\Http\Requests\Matching\DoLikeRequest;
 use App\Business\Matching\CreateReactionService;
 use App\Repository\Enum\ReactionEnum;
+use App\Repository\Reaction;
+use App\Repository\Matching;
 
 
 class DoLikeController extends Controller
@@ -24,15 +26,24 @@ class DoLikeController extends Controller
 
     public function doLike(DoLikeRequest $request)
     {
-        $loginUserID = Auth::user()->id;
+        //ログインユーザー
+        $fromUserID = Auth::user()->id;
         $toUserID = $request->getToUserID();
         $reactionStatus = ReactionEnum::LIKE;
         $createReaction = new CreateReactionService;
-        $createReaction->doCreate($loginUserID, $toUserID, $reactionStatus);
+        $createReaction->doCreate($fromUserID, $toUserID, $reactionStatus);
+        $sportID = 1;
+        $categoryID = 1;// TODO: スポーツとカテゴリーを定義する
+        //いいねのタイミングですでに同じ相手からいいねがあればマッチングさせる
+        $like = Reaction::getByFromAndToID($fromUserID, $toUserID);
+        if($like){
+            // MEMO: マッチングした2人のユーザーをどうやってtrainerとtraineeにどう振り分けるか、決めるまで一旦固定で
+            Matching::insert($fromUserID, $toUserID, $sportID, $categoryID);
+            return View::make('matching.matched');
+        }
         return View::make('matching.result');
     }
 
-    const TABLE_NAME = 'users';
 }
 
 
